007aa570fdf5a7a20a53357c665d64f176581e71
Ishan942 <ishansirdeshpande@gmail.com>
2024-12-28 14:46:25 +0530

    this is an edge case

--- DevTinderBackEnd/src/app.js
+++ DevTinderBackEnd/src/app.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("this is my first response");
+// this will catch all user requets nothing will got to get post delete
+app.use("/user", (req,res)=> {
+    res.send("All request are sent here");
+})
+
+app.get("/user", (req,res)=> {
+    res.send("User Data Feteched");
+})
+
+app.post("/user", (req,res)=> {
+    res.send("User created and added to database");
+})
+
+app.delete("/user", (req,res)=> {
+    res.send("USER deleted successfully");
 })
 
 app.listen(3000, () => {
