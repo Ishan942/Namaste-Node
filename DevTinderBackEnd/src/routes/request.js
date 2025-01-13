5872462cde6dd55c0f16991656a821391232d673
Ishan942 <ishansirdeshpande@gmail.com>
2025-01-13 11:27:50 +0530

    request schema created and connection request works validation remaning

--- DevTinderBackEnd/src/routes/request.js
+++ DevTinderBackEnd/src/routes/request.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const {userAuth} = require("../middlewares/auth")
-
+const ConnectionRequest = require("../models/request");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest",userAuth,  async (req, res) => {
+requestRouter.post("/request/send/:status/:userId",userAuth,  async (req, res) => {
     try{
-        const fName = req.user.firstName;
-        console.log("Connection request sent");
-        res.send(fName+ " sent a connection request");
+        const toUserId = req.params.userId;
+        const fromUserId = req.user._id;
+        const status = req.params.status;
+
+        const connectionRequest = new ConnectionRequest({
+            toUserId,
+            fromUserId,
+            status
+        });
+        const data = await connectionRequest.save();
+        res.json({
+            message: "Connection request updated to "+ status ,
+            data
+        })
+
     } catch(error) {
         res.status(400).send("Error Validating User: "+ error.message);
     }
